b7192df37a757fc1038f69aced0351d7a8b05787
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-09 14:25:57 -0500

    create table pm_metric_parents

--- src/SQL/ddl.sql
+++ src/SQL/ddl.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `pm_metric_parents`;
 DROP TABLE IF EXISTS `pm_metrics`;
 
 CREATE TABLE IF NOT EXISTS `pm_metrics` (
@@ -10,3 +11,13 @@ CREATE TABLE IF NOT EXISTS `pm_metrics` (
   `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 ) AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `pm_metric_parents` (
+
+  `pid` INT NOT NULL,
+  FOREIGN KEY (`pid`) REFERENCES `pm_metrics`(`id`),
+  `cid` INT NOT NULL,
+  FOREIGN KEY (`cid`) REFERENCES `pm_metrics`(`id`),
+  PRIMARY KEY (`pid`,`cid`)
+
+);
